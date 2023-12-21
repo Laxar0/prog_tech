5a747b01c7fbde3d1658d06b0cdd4c78a447d99e
Laxar0 <94480784+Laxar0@users.noreply.github.com>
2023-12-22 01:54:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,49 +1,64 @@
 terraform {
-    required_version = ">=0.13.0"
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 3.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.28.0"
     }
   }
 }
-  provider "aws" {
-    region = "eu-central-1"
-  }
 
-  resource "aws_security_group" "web_app" {
-    name        = "web_app"
-    description = "security group"
+# Configure the AWS provider
+provider "aws" {
+  region     = "eu-central-1"
+}
 
-    ingress {
-      from_port   = 50
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+variable "REPOSITORY_URI" {
+  type = string
+}
 
-    egress {
-      from_port   = 0
-      to_port     = 65535
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags= {
-      Name = "web_app"
+resource "aws_lightsail_container_service" "flask_application" {
+  name = "flask-app"
+  power = "nano"
+  scale = 1
+
+  private_registry_access {
+    ecr_image_puller_role {
+      is_active = true
     }
   }
 
-  resource "aws_instance" "webapp_instance" {
-    ami           = "ami-0669b163befffbdfc"
-    instance_type = "t2.micro"
-    security_groups = ["web_app"]
 
-    tags = {
-      Name = "webapp_instance"
+  tags = {
+    version = "1.0.0"
+  }
+}
+
+resource "aws_lightsail_container_service_deployment_version" "flask_app_deployment" {
+  container {
+    container_name = "flask-application"
+
+    image = "${var.REPOSITORY_URI}:latest"
+    
+    ports = {
+      # Consistent with the port exposed by the Dockerfile and app.py
+      8080 = "HTTP"
     }
   }
 
-  output "instance_public_ip" {
-    value     = aws_instance.webapp_instance.public_ip
-    sensitive = true
+  public_endpoint {
+    container_name = "flask-application"
+    # Consistent with the port exposed by the Dockerfile and app.py
+    container_port = 8080
+
+    health_check {
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      timeout_seconds     = 2
+      interval_seconds    = 5
+      path                = "/"
+      success_codes       = "200-499"
+    }
   }
+
+  service_name = aws_lightsail_container_service.flask_application.name
+}
